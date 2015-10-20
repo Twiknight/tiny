c14a068b0312f4cec4bb00fcf36d0f8fa42232bd
Twiknight <twiknight@163.com>
2015-10-20 16:37:44 +0800

    support https, add remarks

--- src/app.js
+++ src/app.js
@@ -1,27 +1,70 @@
-import http from 'http'
+import http from 'http';
+import https from 'https';
 
 class Tiny {
-    constructor() {
-        this.server = http.createServer();
-    }
-    register(func){
-        if(typeof func !=='function'){
-            throw new TypeError(`register must accept a function as parameter but get ${typeof func}.`);
-        }
 
-        this.server.on('request',func);
+  /*
+   * create a Tiny instance.
+   * you need credential options to set up a https server.
+   * Example:
+   *
+   *   const fs = require('fs');
+   *
+   *   const options = {
+   *      key: fs.readFileSync('test/fixtures/keys/agent2-key.pem'),
+   *      cert: fs.readFileSync('test/fixtures/keys/agent2-cert.pem')
+   *   };
+   *   let tiny = new Tiny(options);
+   *
+   * For http servers:
+   *   let tiny = new Tiny();
+   *
+   * @param {Object} httpsOptions
+   */
+  constructor(httpsOptions) {
+    //property declaration
+    this.server = undefined;
+    this.requestHandler = undefined;
+
+    if(httpsOptions && typeof httpsOptions === 'object'){
+      this.server = https.createServer(httpsOptions);
     }
-    listen(port){
-        let _port = parseInt(port, 10);
-        if(isNaN(_port)){
-            throw new TypeError(`listen must accept a integer as parameter but get ${typeof port}`);
-        }
-        try{
-            this.server.listen(_port);
-        }catch(e){
-            throw e;
-        }
+    else{
+      this.server = http.createServer();
     }
+  }
+
+  /*
+   * bind a hanler function to the server's 'request' event.
+   *
+   * @param {Function} func
+   * @public
+   */
+  register(func){
+    if(typeof func !=='function'){
+      throw new TypeError(`Tiny.register must accept a function as parameter but get ${typeof func}.`);
+    }
+
+    this.server.on('request',func);
+  }
+
+  /*
+   * listen to a port for http requests
+   *
+   * @param {Number} port
+   * @public
+   */
+  listen(port){
+    let _port = parseInt(port, 10);
+    if(isNaN(_port)){
+        throw new TypeError(`Tiny.listen must accept a integer as parameter but get ${typeof port}`);
+    }
+    try{
+        this.server.listen(_port);
+    }catch(e){
+        throw e;
+    }
+  }
 }
 
 export default Tiny;
